f25df3433616350a484fd5b61b20bdee4453e435
Sagiri721 <tiagobarrossao@gmail.com>
2022-08-07 16:06:39 +0100

    refactor queu

---
+++ Assets/Objects/Player.java
@@ -0,0 +1,27 @@
+package Assets.Objects;
+
+import java.awt.Graphics2D;
+
+import Engine.Utils.StdBehaviour;
+
+public class Player extends Object implements StdBehaviour {
+
+    @Override
+    public void Start() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void Update(float deltaTime) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void DrawGUI(Graphics2D g) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
