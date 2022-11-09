a406189e6e1e4dbb53781b494d6409b28852c82e
Sagiri <tiagobarrossao@gmail.com>
2022-11-09 16:28:47 +0000

    updated project

--- Main.java
+++ Main.java
@@ -18,16 +18,5 @@ public class Main extends HimawariCore{
     public static void main(String[] args) {
 
         CreateWindow(800, 600, "Nice Game");
-
-        //CreateObject("Wall", new Vec2(-200, 400), 0, new Vec2(40, 7));
-        //CreateObject("Wall", new Vec2(100, 300), 0, new Vec2(1,1));
-
-        TileSet set = new TileSet(new Sprite("Grass.png"), 16, 16);
-        Room r = new Room(set,  new RoomData("room1"));
-
-        LoadRoom(r);
-
-        CreateObject("Ball", new Vec2(40,0), 0, new Vec2(1, 1));
-        CreateObject("GameCamera", Vec2.ZERO, 0, Vec2.ZERO);
     }
 }
